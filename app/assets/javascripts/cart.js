72e740b5f80799e42d476d4d99621708145ad7d6
Daniel Pereira <pereiradpg@yahoo.ca>
2015-11-06 11:45:00 -0500

    Index styles set, cart info bar is fixed to bottome of window

---
+++ app/assets/javascripts/cart.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   // Only trigger if the element is fixed
+   $('.cart-info-bar').width($('.container').width());
+});
+
+$(window).resize(function(){
+   // Only trigger if the element is fixed
+   $('.cart-info-bar').width($('.container').width());
+});
