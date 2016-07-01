516560c79603600f26af4b5a97959cd76d6b76d1
Daniel <truenorthtrainingcompany@gmail.com>
2016-07-01 14:07:38 -0400

    LCBOProduct class returns API results

--- app/models/lcboproduct.rb
+++ app/models/lcboproduct.rb
@@ -1,4 +1,13 @@
 class Lcboproduct
 	include HTTParty
-	
+	base_uri 'lcboapi.com'
+
+	def self.all
+		get('/products')['result']
+	end
+
+	def self.find(product_id)
+		get('/products/#{product_id}')['result']
+	end
+
 end
